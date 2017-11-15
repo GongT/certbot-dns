28c317422f43f161e96a77baf4369def4885e83f
GongT <admin@gongt.me>
2017-11-15 19:49:10 +0800

    finish

--- get-cert/put_dns_config.sh
+++ get-cert/put_dns_config.sh
@@ -2,11 +2,17 @@
 
 set -e
 
+trap "echo GOT HUP" SIGHUP
+
 source base.sh
 
 export FILE_NAME="$1"
 if [ -z "${FILE_NAME}" ]; then
 	die "Error: no file name"
+fi
+export CONTENT_FROM="$2"
+if [ -n "${CONTENT_FROM}" ] && ! [ -e "${CONTENT_FROM}" ]; then
+        die "Error: no source file: ${CONTENT_FROM}"
 fi
 
 if false ; then
@@ -27,14 +33,14 @@ function export_var() {
 	done
 }
 
+
 # export_var DNSMASQ_CONFIG_DIR FILE_NAME DNSMASQ_SERVICE_CONTROL > ${TMP}
 cat > ${TMP} << 'InputComesFromHERE'
 set -e
-set -x
 F="${DNSMASQ_CONFIG_DIR}/${FILE_NAME}.conf"
 mkdir -p "${DNSMASQ_CONFIG_DIR}"
 echo "# GENERATED FILE DO NOT MODIFY" > "${F}"
-cat /dev/stdin > "${F}"
+tee >> "${F}"
 if [ "$(id -u)" -ne 0 ]; then
 	export sudo="sudo"
 fi
@@ -46,17 +52,21 @@ $sudo ${DNSMASQ_SERVICE_CONTROL} || {
 exit 0
 InputComesFromHERE
 
-
+function stdin() {
+	if [ -z "${CONTENT_FROM}" ]; then
+		cat
+	else
+		cat "${CONTENT_FROM}"
+	fi
+}
 
 if [ -z "$DNS_REMOTE" ]; then
-	cat /dev/stdin | bash ${TMP} || die "not able to apply dns config"
+	stdin | tee /dev/stderr | bash ${TMP} || die "not able to apply dns config"
 else
 	ARGS=("${DNS_REMOTE}")
 	if [ -n "${DNS_REMOTE_KEYFILE}" ]; then
 		ARGS+=(-i "${DNS_REMOTE_KEYFILE}")
 	fi
-	set -x
-	cat /dev/stdin | expect auto_ssh.expect "${ARGS[@]}" bash -c "$(<"${TMP}")" || die "not able to apply dns config"
-	set +x
+	stdin | tee /dev/stderr | expect auto_ssh.expect "${ARGS[@]}" bash -c "$(<"${TMP}")" || die "not able to apply dns config"
 fi
 
